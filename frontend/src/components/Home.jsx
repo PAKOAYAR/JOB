a4cd8e8d297f21264028807adb81c0fa19681595
PAKOAYAR <godhampankaj111@gmail.com>
2024-10-16 22:42:41 +0530

    first

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './ui/shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
 
 const Home = () => {
   return (
     <div>
         <Navbar></Navbar>
+        <HeroSection/>
+      <CategoryCarousel/>
+    
+        {/* <LatestJobs/>
+        <Footer/>  */}
     </div>
   )
 }
