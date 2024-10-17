0182396e172c2030bc00d85fc07f945b23685e84
PAKOAYAR <godhampankaj111@gmail.com>
2024-10-17 16:33:22 +0530

    flitering for jobs

--- frontend/src/components/FilterCards.jsx
+++ frontend/src/components/FilterCards.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
 const filterData=[
     {
         filterType:"location",
@@ -15,9 +17,30 @@ const filterData=[
 ]
 const FilterCards = () => {
   return (
-    <div>
-        <h1>Filter Jobs</h1>
+    <div className='w-full bg-white p-3 rounded-md'>
+        <h1 className='font-bold text-lg'>Filter Jobs</h1>
         <hr className='mt-3 '/>
+        <RadioGroup>
+            {
+                filterData.map((data,index)=>(
+                    <div>
+                        <h1 className='font-bold text-lg'>
+                            {data.filterType}
+                            {
+                                data.array.map((item,index)=>{
+                                    return(
+                                        <div className='flex items-center space-x-2 my-2'>
+                                            <RadioGroupItem value={item}/>
+                                            <Label>{item}</Label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </h1>
+                    </div>
+                ))
+            }
+        </RadioGroup>
     </div>
   )
 }
