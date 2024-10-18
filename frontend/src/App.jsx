bfaa10a416f923bf10534467bf56096860729589
PAKOAYAR <godhampankaj111@gmail.com>
2024-10-18 10:34:38 +0530

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Jobs from './components/jobs'
 import Browse from './components/Browse'
+import Profile from './components/Profile'
 //import Navbar from './components/ui/shared/Navbar'
 const appRouter=createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const appRouter=createBrowserRouter([
   {
     path:'/browse',
     element:<Browse/>
+  },
+  {
+    path:'/profile',
+    element:<Profile/>
   }
 ])
 function App() {
